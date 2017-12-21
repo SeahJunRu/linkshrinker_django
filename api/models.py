249576e2bb3c78a958193e52e33c03ea0da0f03a
SeahJunRu <junru1231@gmail.com>
2017-12-21 09:54:13 +0000

    adding model in api

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# api/models.py
+
+class URL(models.Model):
+    URL = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return self.URL
+    
